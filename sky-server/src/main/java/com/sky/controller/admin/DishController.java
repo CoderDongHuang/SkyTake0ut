d2f5b1d39f8fbe4356333d204087eae4028b8f64
CoderDongHuang <1218798773@qq.com>
2025-12-05 21:29:25 +0800

    删除菜品业务代码实现

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.sky.service.DishService;
 
+import java.util.List;
+
 /**
  * 菜品管理
  */
@@ -49,4 +51,18 @@ public class DishController {
         PageResult pageResult  = dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
     }
+
+    /**
+     * 批量删除
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("菜品批量删除")
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("菜品批量删除：{}", ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
+    }
+
 }
